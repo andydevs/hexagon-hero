7d0b5841641041fb279a81c678a6e7e333689618
Andydevs <akanshul97@gmail.com>
2021-05-16 22:59:10 -0400

    Bestagon

--- app/index.js
+++ app/index.js
@@ -5,99 +5,98 @@
  * Created: [Creation Date]
  */
 import './style/main.scss'
-import Vector from './vector';
-import HMatrix from './hmatrix';
-import { Transform, Hexagon } from './hexagon';
-import { fromEvent } from "rxjs";
-import { debounceTime } from "rxjs/operators";
+import Two from "two.js";
+
+// Parameters
+const timeRate = 0.01
+const preAlpha = 0.90
+const radius = 50
+
+// Helper constants
+const tx = (1 + Math.cos(Math.PI/3))
+const ty = Math.sin(Math.PI/3)
+const rx = radius*tx
+const ry = radius*ty
 
 // Get canvas and set width height
-let canvas = document.querySelector('#canvas')
-canvas.width = canvas.clientWidth
-canvas.height = canvas.clientHeight
-let ctx = canvas.getContext('2d')
+let app = document.querySelector('#app-root')
+let two = new Two({
+    width: app.clientWidth,
+    height: app.clientHeight
+}).appendTo(app)
+window.addEventListener('resize', function() {
+    two.width = app.clientWidth
+    two.height = app.clientHeight
+})
 
-// Origin transform
-const OX = canvas.width/2
-const OY = canvas.height/2
-const OT = new HMatrix(
-    1, 0, OX,
-    0, -1, OY
-)
 
-/**
- * Draw polygon to screen
- * 
- * @param {Polygon} polygon the polygon to draw 
- */
-function drawPolygon(polygon) {
-    // Get parameters
-    let vertices = polygon.vertices
-    let fill = polygon.fill
-
-    // Transform vertices into screenspace
-    // Get start point and also put it at the end
-    let screenpoints = OT.transformMany(vertices)
-    let start = screenpoints.shift()
-    screenpoints.push(start)
-
-    // Draw path
-    ctx.beginPath()
-    ctx.fillStyle = fill
-    ctx.moveTo(start.x, start.y)
-    for (const point of screenpoints) {
-        ctx.lineTo(point.x, point.y)
-    }
-    ctx.fill()
+// Ease in out function
+// 
+// Credit to: https://easings.net/#easeInOutExpo
+// Created by: Andrey Sitnik and Ivan Solovev
+// For the function
+const easeInOut = x => {
+    return x < 0.5 
+        ? Math.pow(2, 20 * x - 10) / 2
+        : (2 - Math.pow(2, -20 * x + 10)) / 2
 }
 
-// Polygon constants
-const tX = 1 + Math.cos(Math.PI/3)
-const tY = Math.sin(Math.PI/3)
-const tDown = new Vector(tX, -tY)
-const tUp = new Vector(tX, tY)
+// Repeating wave pattern of signal with easing
+const upDownSignal = (totalTime, time) => {
+    let m = 4 * (time % totalTime) / totalTime
+    return m < 1
+        ? easeInOut(m % 1)
+        : m < 2
+        ? 1
+        : m < 3
+        ? 1 - easeInOut(m % 1)
+        : 0
+}
 
-// Polygon params
-const rad = 30
-const rDown = tDown.scale(rad)
-const rUp = tUp.scale(rad)
+// Repeating stair-step pattern of signal with easing
+const upUpSignal = (totalTime, time) => {
+    let m = 4 * (time % totalTime) / totalTime
+    return m < 1
+        ? easeInOut(m % 1)
+        : m < 2
+        ? 1
+        : m < 3
+        ? 1 + easeInOut(m % 1)
+        : 2
+}
 
-function drawTile() {
-    // Clear rectangle
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
+// Animation loop
+let t = 0
+two.bind('update', function update() {
+    two.clear()
 
-    // Create polygon
-    let polygon = new Hexagon(new Transform(rad*0.99))
+    // Row parameters
+    let r, j
+    for (let y = 0; y < two.height + radius; y += 2*ry) {
+        // Init parameters
+        r = y
+        j = 1
 
-    // Get number in each row and column
-    let numX = Math.ceil(ctx.canvas.width / (rad * tX))
-    let numY = Math.ceil(ctx.canvas.height / (rad * tY))
+        // Go through row
+        for (let x = 0; x < two.width + radius; x += rx) {
+            // Calculate offset parameter
+            let a = (y/two.height + x/two.width)/2 + t
+            let scale = preAlpha*upDownSignal(20, a)
+            let rotation = 2*Math.PI*upUpSignal(20, a)
 
-    // Draw row
-    for (let jndex = 0; jndex < numY; jndex++) {
-        // Move to position
-        polygon.transform.position = new Vector(-OX, OY - 2*tY*jndex*rad)
-        drawPolygon(polygon)
+            // Create polygon
+            let poly = two.makePolygon(x, r, radius, 6)
+            poly.scale = scale
+            poly.rotation = rotation
+            poly.fill = '#00aaff'
 
-        // Draw hexes in pairs (repeating pattern)
-        for (let index = 0; index < numX; index++) {
-            polygon.transform.translate(rDown)
-            drawPolygon(polygon)
-            polygon.transform.translate(rUp)
-            drawPolygon(polygon)
+            // Update parameters
+            r += j*ry
+            j *= -1
         }
     }
-}
-drawTile()
 
-// Observable magic
-fromEvent(window, 'resize')
-    .pipe(debounceTime(100))
-    .subscribe(() => {
-        // Reset width height
-        canvas.width = canvas.clientWidth
-        canvas.height = canvas.clientHeight
-
-        // Draw tile
-        drawTile()
-    })
+    // Increment time
+    t += timeRate
+})
+two.play()
